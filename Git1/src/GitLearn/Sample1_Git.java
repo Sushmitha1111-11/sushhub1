3e7d6206fd10fa22f848dd02a9b618e7104b5b68
91957 <91957@DESKTOP-F76IHI6>
2020-06-20 20:34:24 +0530

    C1

---
+++ Git1/src/GitLearn/Sample1_Git.java
@@ -0,0 +1,10 @@
+package GitLearn;
+
+public class Sample1_Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is sample of git1");
+	}
+
+}
